70f166a34a1026f1e23b4c369d22d96a032823f3
Tian Xing <xingpicture@gmail.com>
2026-08-06 13:09:38 -0400

    Silence expected intro hydration difference

--- app/layout.tsx
+++ app/layout.tsx
@@ -51,7 +51,7 @@ export const viewport: Viewport = {
 
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <head>
         <script dangerouslySetInnerHTML={{ __html: phoneIntroBootstrap }} />
       </head>
